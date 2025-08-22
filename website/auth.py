50a256ca70a45190b32c7fdde5779b085d25ad93
notsoworkingbro <workingamap@gmail.com>
2025-08-22 19:07:04 +0800

    change generate pasword hash method

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,28 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import User 
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p> Login </p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('logged in succesful', category='success')
+            else:
+                flash('Incorrect Password , try again', category='error')
+
+        else:
+            flash('user does not exist')
+
+
+    return render_template("login.html", show_navbar=False)
 
 @auth.route('/logout')
 def logout():
@@ -19,10 +37,28 @@ def sign_up():
         confirm = request.form.get('confirm')
 
         # Just simple validation (you can expand later)
-        if password != confirm:
+
+        user = User.query.filter_by(email=email).first()
+        if  user:
+            flash('Email already exist', category='error')
+
+        elif password != confirm:
             flash('Passwords do not match!', category='error')
+
         else:
             flash('Account created successfully!', category='success')
+
+            # Create new user
+            new_user = User(
+                email=email,
+                username=username,
+                password=generate_password_hash(password, method='pbkdf2:sha256')
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash('Account created successfully!', category='success')
+
             return redirect(url_for('views.home'))
 
     return render_template('sign_up.html', show_navbar=False)
